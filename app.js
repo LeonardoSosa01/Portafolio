e8cd04ac36f1479198b7b3ba0e5c96b20c8d6dd3
leonardososa01 <leitoosg@gmail.com>
2024-02-29 21:34:03 -0300

    retomando proyecto

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // ACTIVIDAD DOM con javascript
 // Primer punto imagen, cambia cuando pases el mouse
 function change1() {
-  document.getElementById("img1").src = "/images/mujermain2.png";
+  document.getElementById("img1").src = "./images/mujermain2.png";
 }
 
 function change2() {
-  document.getElementById("img1").src = "/images/mujermain.png";
+  document.getElementById("img1").src = "./images/mujermain.png";
 }
 
 // Segundo punto, agregar ver mas, mas texto y dos botones, un exit y un link al repo
